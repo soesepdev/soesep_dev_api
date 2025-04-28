798a626a3cfb752babc2f37c16128a0865016e68
Soesep Dev <soesep.dev@gmail.com>
2025-04-28 14:56:48 +0700

    update : add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const routes = require('./routes');
 
 app.use(express.json());
+app.use(cors());
 app.use('/', routes);
 
 const PORT = process.env.PORT || 5000;
